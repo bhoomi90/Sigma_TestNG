01852ee2e676f2942f4ba28369de6b4cb4d4b0e7
bhoomi <bhoomi.chakrani@hotmail.com>
2025-05-14 14:20:55 -0400

    All empty classes

--- src/test/java/utilities/LoggerLoad.java
+++ src/test/java/utilities/LoggerLoad.java
@@ -3,7 +3,7 @@ package utilities;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class LoggerReader {
+public class LoggerLoad {
 
 	private static Logger logger = LogManager.getLogger();
 	
