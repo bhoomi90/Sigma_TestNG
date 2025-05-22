132fd2e583e553baa172c61d07eb4c00c6d57603
vahinig03 <vahini.r44@gmail.com>
2025-05-22 13:33:30 -0400

    Update HomePage.java

--- src/test/java/pageFactory/HomePage.java
+++ src/test/java/pageFactory/HomePage.java
@@ -1,65 +1,29 @@
+
 package pageFactory;
 
-
-    import java.util.List;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import utilities.LoggerLoad;
 import webdriver.DriverFactory;
 
- public class HomePage {
-
-		public HomePage() {
-			PageFactory.initElements(DriverFactory.getdriver(), this);
-		}
-
-		@FindBy(className = "btn" ) 
-		private WebElement getStartedBtn;
-		// Home Page Elements
-		@FindBy(xpath = "//a[text()='NumpyNinja']")
-		private WebElement numpyNinjaLink;
-
-		@FindBy(xpath = "//a[contains(text(), 'Sign in')]")
-		private WebElement signInLink;
-
-		@FindBy(xpath = "//a[contains(text(), 'Register')]")
-		private WebElement registerLink;
-
-		@FindBy(xpath = "//a[@class='nav-link dropdown-toggle']")
-		private WebElement dsDropdown;
-
-		@FindBy(xpath = "//div[@class='dropdown-menu show']//a")
-		private List<WebElement> dsDropdownOptions;
-
-		@FindBy(xpath = "//div[contains(text(),'You are not logged in')]")
-		private WebElement errMsg;
-
-		@FindBy(xpath = "//a[text()='NumpyNinja' or text()='Data Structures' or normalize-space(text())='Register' or text()='Sign in']")
-		private List<WebElement> homeHeaderLinks;
-
+public class HomePage {
 	
-		public void getStartedBtnClick() {
-			getStartedBtn.click();
-		}
-		
+	@FindBy (xpath=("//*[text()='Get Started']")) WebElement getStarted;
+	@FindBy (xpath=("//*[text()='Sign in']")) WebElement signIn ;
 
-		public void clickNumpyNinja() {
-			numpyNinjaLink.click();
-		}
-
-		public void signInLinkClick() {
-			signInLink.click();
-		}
-
-		public void RegisterLinkClick() {
-			registerLink.click();
-		}
-
-		
-
+	public HomePage() {
+		PageFactory.initElements(DriverFactory.getdriver(), this);
+		LoggerLoad.info("Initialized Home Page");
+	}
+	
+	public void clickgetStarted() {
+		LoggerLoad.info("Clicking get started button in Home page");
+		getStarted.click();
+	}
+	public void clickSignIn() {
+		LoggerLoad.info("Clicking signIn button in Home page");
+		signIn.click();
+	}
 }
-
