f0ca2355607c33ff32235da85e07b645d4a00931
bhoomi <bhoomi.chakrani@hotmail.com>
2025-05-22 23:26:51 -0400

    Validate login and register

--- src/test/java/pageFactory/RegisterPage.java
+++ src/test/java/pageFactory/RegisterPage.java
@@ -1,5 +1,76 @@
 package pageFactory;
 
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import utilities.CommonMethods;
+import utilities.LoggerLoad;
+import webdriver.DriverFactory;
+
 public class RegisterPage {
+
+	@FindBy(xpath = ("//*[text()='Get Started']"))
+	WebElement getStarted;
+	@FindBy(xpath = ("//a[text()=' Register ']"))
+	WebElement registerInitial;
+	@FindBy(id = "id_username")
+	WebElement username;
+	@FindBy(id = "id_password1")
+	WebElement password;
+	@FindBy(id = "id_password2")
+	WebElement passwordConfirmation;
+	@FindBy(xpath = ("//input[@type='submit' and @value='Register']"))
+	WebElement registerBttn;
+	@FindBy(xpath = ("//*[@role='alert']"))
+	WebElement alertMsg;
+
+	String errorText;
+
+	public RegisterPage() {
+		PageFactory.initElements(DriverFactory.getdriver(), this);
+		LoggerLoad.info("Initialized Register Page");
+	}
+
+	public void getStarted() {
+		getStarted.click();
+	}
+
+	public void register_initial() {
+		registerInitial.click();
+		LoggerLoad.info("Clicking Register button");
+	}
+
+	public void enterfields(String usernameInput, String passwordInput, String passwordConfirm) {
+		username.sendKeys(usernameInput);
+		password.sendKeys(passwordInput);
+		passwordConfirmation.sendKeys(passwordConfirm);
+		registerBttn.click();
+	}
+
+	public String getEmptyUsernameAlertMsg() {
+		JavascriptExecutor js = (JavascriptExecutor) DriverFactory.getdriver();
+		String validationMsg = (String) js.executeScript("return arguments[0].validationMessage;", username);
+		return validationMsg;
+	}
+
+	public String getEmptyPasswordAlertMsg() {
+		JavascriptExecutor js = (JavascriptExecutor) DriverFactory.getdriver();
+		String validationMsg = (String) js.executeScript("return arguments[0].validationMessage;", password);
+		return validationMsg;
+	}
+
+	public String getEmptyConfirmPasswordAlertMsg() {
+		JavascriptExecutor js = (JavascriptExecutor) DriverFactory.getdriver();
+		String validationMsg = (String) js.executeScript("return arguments[0].validationMessage;", passwordConfirmation);
+		return validationMsg;
+	}
+
+	public String readErrorText() {
+		CommonMethods.waitForElementTobeClick(alertMsg);
+		errorText = alertMsg.getText();
+		return errorText;
+	}
 
 }
--- src/test/java/testClasses/LoginTest.java
+++ src/test/java/testClasses/LoginTest.java
@@ -1,10 +1,59 @@
 package testClasses;
 
+import org.testng.annotations.Test;
 
+import hooks.Hooks;
+import utilities.LoggerLoad;
+import utilities.TestDataProvider;
 
-public class LoginTest  {
+public class LoginTest extends Hooks {
 	
-	
+	@Test (priority = 0)
+	public void navigateLoginPage() {
+		LoggerLoad.info("Let's login to DsPortal App from login testclass");
+		pfm.getHomePage().clickgetStarted();
+		pfm.getHomePage().clickSignIn();
+	}
 
+	@Test (priority = 1, dataProvider = "InvalidUserLoginData", dataProviderClass = TestDataProvider.class)
+	public void invalidValidField(String username, String password, String message) {
+		pfm.getLoginPage().enterfields(username, password);
+		LoggerLoad.info("Entered data with invalid username: " +username+ " valid password" +password);
+		String actualMsg = pfm.getLoginPage().readErrorText();
+		String expectedMSG = message; 
+		softAssert.assertEquals(actualMsg, expectedMSG, "Error message is not appeared");
+		
+	}
+	
+	@Test (priority = 2, dataProvider = "InvalidPWLoginData", dataProviderClass = TestDataProvider.class)
+	public void validInvalidField(String username, String password, String message) {
+		driver.navigate().refresh();
+		pfm.getLoginPage().enterfields(username, password);
+		LoggerLoad.info("Entered data with valid username: " +username+ " invalid password" +password);
+		String actualMsg = pfm.getLoginPage().readErrorText();
+		String expectedMSG = message; 
+		softAssert.assertEquals(actualMsg, expectedMSG, "Error message is not appeared");
+	}
+	
+	@Test (priority = 3, dataProvider = "EmptyUserLoginData", dataProviderClass = TestDataProvider.class)
+	public void emptyValidField(String username, String password, String message) {
+		driver.navigate().refresh();
+		pfm.getLoginPage().enterfields(username, password);
+		LoggerLoad.info("Entered data with empty username: " +username+ " valid password" +password);
+		String actualMsg = pfm.getLoginPage().getEmptyUserNameAlertMsg();
+		String expectedMSG = message ; 
+		softAssert.assertEquals(actualMsg, expectedMSG, "Error message is not appeared under username field");
+	}
+	
+	@Test (priority = 4, dataProvider = "EmptyPWLoginData", dataProviderClass = TestDataProvider.class)
+	public void validEmptyField(String username, String password, String message) {
+		driver.navigate().refresh();
+		pfm.getLoginPage().enterfields(username, password);
+		LoggerLoad.info("Entered data with valid username: " +username+ " empty password" +password);
+		String actualMsg = pfm.getLoginPage().getEmptyPasswordAlertMsg();
+		String expectedMSG = message; 
+		softAssert.assertEquals(actualMsg, expectedMSG, "Error message is not appeared under password field");
+		softAssert.assertAll();
+	}
 }
 
